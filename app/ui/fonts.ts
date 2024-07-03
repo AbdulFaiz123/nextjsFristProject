7e9f9d72b52357ad67dd007133c9f256f32cc186
Abdul Faiz <abdul.faiz0010@gmail.com>
2024-07-03 12:42:01 +0200

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Fontdiner_Swanky, Inter } from 'next/font/google';
+
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+
+export const lusitana = Lusitana({ weight : ['400','700'] ,subsets : ['latin'] })
+
